7665e06ae3d938b4e83733368d73f18bd38b25ab
oscarnyquist <37046485+oscarnyquist@users.noreply.github.com>
2018-12-21 19:17:23 +0100

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
 @echo off
-cd C:\Users\Oscar\Desktop\raspberry\Boot GUI
 set FLASK_APP=flaskapp.py
 flask run
--- start_debug_error.bat
+++ start_debug_error.bat
@@ -1,5 +1,4 @@
 @echo off
-cd C:\Users\Oscar\Desktop\raspberry\Boot GUI
 set FLASK_APP=flaskapp.py
 python -m flask run
 pause
